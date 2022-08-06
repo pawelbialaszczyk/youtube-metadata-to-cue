09ae17b570e423f031271377356468e472e6a30d
Paweł Białaszczyk <pawelbialaszczyk@gmail.com>
2022-08-06 19:12:58 +0200

    Add test case name

--- tests/convert.spec.js
+++ tests/convert.spec.js
@@ -2,25 +2,21 @@ const fs = require('fs');
 const path = require('path');
 const convert = require('../src/convert');
 
+const testDataDir = path.join(__dirname, 'data');
+
+const readTestNames = () =>
+  new Set(fs.readdirSync(testDataDir).map(file => path.parse(file).name));
+
+const readTestFile = fileName =>
+  fs.readFileSync(path.join(testDataDir, fileName), 'utf8');
+
 describe('convert', () => {
-  const testDataDir = path.join(__dirname, 'data');
+  const testCases = Array.from(readTestNames());
 
-  const testCases = fs.readdirSync(testDataDir)
-    .map(path.parse)
-    .reduce((cases, { name, ext, base }) => {
-      const file = fs.readFileSync(path.join(testDataDir, base), 'utf8');
+  it.each(testCases)('should map JSON to CUE for %s', name => {
+    const json = JSON.parse(readTestFile(`${name}.json`));
+    const cue = readTestFile(`${name}.cue`);
 
-      cases[name] = cases[name] ?? [];
-
-      switch (ext) {
-        case '.json': cases[name][0] = JSON.parse(file); break;
-        case '.cue': cases[name][1] = file; break;
-      }
-
-      return cases;
-    }, {});
-
-  it.each(Object.values(testCases))('should map JSON to CUE', (json, cue) => {
     expect(convert(json)).toBe(cue);
   });
 });
